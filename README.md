d412a5b261ecb20d4f1581360d3da253547333fa
RightNow <daniel.benner02@gmail.com>
2021-09-17 13:27:49 +0200

    V2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-"# SolarsystemWebsite" 
+### About: 
+```bash
+This is a project I´m working on while leraning Java Script and a bit of web development.
+Using Three Js I´m trying to recreate our solarsystem and show the simulation on a selfhosted website,
+Work in progress!
+```
--- SolarsystemWebsite/main_bu.js
+++ SolarsystemWebsite/main_bu.js
@@ -6,42 +6,39 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
 var renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'), antialias: true});
-var orbits = [];
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+
+var sunTex = new THREE.TextureLoader().load('textures/2k_sun.jpg');
+var sun = new THREE.Mesh(
+  new THREE.SphereGeometry(0.139, 24, 18, 0, Math.PI * 2, 0, Math.PI),
+  new THREE.MeshStandardMaterial({map:sunTex})
+);
+scene.add(sun);
+
+createPlanet('Mercury', 0.00049, 5.7);
+createPlanet('Venus', 0.0012, 10.8);
+createPlanet('Earth', 0.0013, 150);
+createPlanet('Mars', 0.00068, 22.7);
+createPlanet('Jupiter', 0.0143, 77.8);
+createPlanet('Saturn', 0.01205, 143.4);
+createPlanet('Uranus', 0.00511, 287);
+createPlanet('Neptun', 0.00495, 449.5);
+
+var planets = [];
+
+
+
+var ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+var gridHelper = new THREE.GridHelper(100, 70);
+//scene.add(lightHelper, gridHelper);
+
 var controls = new OrbitControls(camera, renderer.domElement);
-var sun, ambientLight, gridHelper;
 
-init();
-animate();
-
-function init(){
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.position.setZ(30);
-
-  var sunTex = new THREE.TextureLoader().load('textures/2k_sun.jpg');
-  sun = new THREE.Mesh(
-    new THREE.SphereGeometry(0.139, 24, 18, 0, Math.PI * 2, 0, Math.PI),
-    new THREE.MeshStandardMaterial({map:sunTex})
-  );
-  scene.add(sun);
-
-  ambientLight = new THREE.AmbientLight(0xffffff);
-  scene.add(ambientLight);
-
-  gridHelper = new THREE.GridHelper(100, 70);
-  //scene.add(gridHelper);
-
-  createPlanet('Mercury', 0.49, 5.7);
-  createPlanet('Venus', 0.0012, 10.8);
-  createPlanet('Earth', 0.0013, 150);
-  createPlanet('Mars', 0.00068, 22.7);
-  createPlanet('Jupiter', 0.0143, 77.8);
-  createPlanet('Saturn', 0.01205, 143.4);
-  createPlanet('Uranus', 0.00511, 287);
-  createPlanet('Neptun', 0.00495, 449.5);
-
-  Array(1000).fill().forEach(addFlyingStuff);
-}
 
 function addFlyingStuff(){
   const geometry = new THREE.SphereGeometry(0.15, 5, 5);
@@ -98,27 +95,22 @@ function createPlanet(name, size, distance){
   const canvas = makeLabelCanvas(150, 50, name);
   const texture = new THREE.CanvasTexture(canvas);
   const planetLabelMaterial = new THREE.SpriteMaterial ({map: texture, transparent: true});
-  var planetLabel = new THREE.Sprite(planetLabelMaterial);
+  const planetLabel = new THREE.Sprite(planetLabelMaterial);
 
   planet.position.set(distance, 0, 0);
   planetRing.rotation.set(90, 0, 0);
   planetLabel.position.set(planet.position.x, planet.position.y, planet.position.z);
   
-  var orbit = new THREE.Group();
-  orbit.add(planet);
-  orbit.add(planetRing);
-  orbits.push(orbit);
-  scene.add(orbit);
+  scene.add(planet, planetRing, planetLabel);
 }
+
+
+Array(1000).fill().forEach(addFlyingStuff);
 
 function animate(){
   requestAnimationFrame(animate);
   controls.update();
   renderer.render(scene, camera);
-
-  date = Date.now() * 1;
-  for (i = 0; i < planets.length; i++){
-    orbits[i].rotation.y += 10;
-  }
 }
 
+animate();
